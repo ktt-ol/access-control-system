2741c46bc59c13e13cc4fa4ae9dd602b9875eed0
Sebastian Reichel <sre@ring0.de>
2016-08-09 01:28:54 +0200

    common/i2c: fix 16bit methods

--- common/i2c.c
+++ common/i2c.c
@@ -35,9 +35,9 @@ int i2c_read(int file, uint8_t reg) {
 }
 
 int i2c_write16(int file, uint8_t reg, uint16_t val) {
-	return i2c_smbus_write_byte_data(file, reg, val);
+	return i2c_smbus_write_word_data(file, reg, val);
 }
 
 int i2c_read16(int file, uint8_t reg) {
-	return i2c_smbus_read_byte_data(file, reg);
+	return i2c_smbus_read_word_data(file, reg);
 }
