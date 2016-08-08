a6ecfa05713d5d4ef8f1bbbc3f5f6612b515eb7a
Sebastian Reichel <sre@ring0.de>
2016-08-09 01:15:13 +0200

    common/i2c: add more helpers

--- common/i2c.c
+++ common/i2c.c
@@ -29,3 +29,15 @@ int i2c_close(int file) {
 int i2c_write(int file, uint8_t reg, uint8_t val) {
 	return i2c_smbus_write_byte_data(file, reg, val);
 }
+
+int i2c_read(int file, uint8_t reg) {
+	return i2c_smbus_read_byte_data(file, reg);
+}
+
+int i2c_write16(int file, uint8_t reg, uint16_t val) {
+	return i2c_smbus_write_byte_data(file, reg, val);
+}
+
+int i2c_read16(int file, uint8_t reg) {
+	return i2c_smbus_read_byte_data(file, reg);
+}
--- common/i2c.h
+++ common/i2c.h
@@ -5,6 +5,9 @@
 
 int i2c_open(int bus, int dev);
 int i2c_close(int file);
+int i2c_read(int file, uint8_t reg);
 int i2c_write(int file, uint8_t reg, uint8_t val);
+int i2c_read16(int file, uint8_t reg);
+int i2c_write16(int file, uint8_t reg, uint16_t val);
 
 #endif
